dc478860190e312701b273cffcb96f556a15201c
mariona081 <xanfonm@gmail.com>
2025-07-16 02:58:23 +0300

    image-centering-0n-hover

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -11,15 +11,17 @@ export default function HomePage() {
         </div>
         <div className="relative z-30">
             <Header/>
-            <section className="flex flex-col group  items-center justify-center text-center h-screen">
-                <div className=" relative">
-                    <img className="w-80 h-auto group-hover:opacity-0 transition-opacity duration-200 cursor-pointer" src="/img/marion.png"/>
-                    <img className="w-90 h-auto absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 cursor-pointer" src="/img/marionHover.png"/>
+            <section className="flex flex-col  items-center justify-center text-center h-screen">
+                <div className=" relative group flex flex-col items-center justify-center">
+                    <div className="relative w-80 ">
+                        <img className="w-80 h-auto group-hover:opacity-0 transition-opacity duration-200 cursor-pointer" src="/img/marion.png"/>
+                        <img className="w-90 h-auto absolute top-0 left-0 opacity-0 group-hover:opacity-100 transition-opacity duration-200 cursor-pointer" src="/img/marionHover.png"/>
+                    </div>
+                    <h6 className="font-thin opacity-100 group-hover:opacity-0 transition-opacity duration-500 mt-6">i'm quietly crafting <span className="text-amber-50 " style={{fontFamily: 'Brightwall'}}>my perfection </span>of the world through <span className="text-amber-50" style={{fontFamily: 'Brightwall'}}>a different lens</span></h6>
+                    <button className="mt-6 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                        <Link to="/aboutMe"  className="py-2 px-6  rounded-full shadow text-amber-900 bg-amber-50 border-0 transition-all durtion-500  font-semibold">Explore my blog</Link>
+                    </button>
                 </div>
-                <h6 className="font-thin opacity-100 group-hover:opacity-0 transition-opacity duration-500 mt-6">i'm quietly crafting <span className="text-amber-50 " style={{fontFamily: 'Brightwall'}}>my perfection </span>of the world through <span className="text-amber-50" style={{fontFamily: 'Brightwall'}}>a different lens</span></h6>
-                <button className="mt-6 opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-                    <Link to="/aboutMe"  className="py-2 px-6  rounded-full shadow text-amber-900 bg-amber-50 border-0 transition-all durtion-500  font-semibold">Explore my blog</Link>
-                </button>
                 <div className="flex items-end justify-center h-40 ">
                     <div className="bg-white h-13 w-13 rounded-full flex items-center justify-center">
                     <Link to="/"><HomeIcon className="h-8 w-8 text-black" /></Link> 
