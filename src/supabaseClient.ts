9109a51097a4a37cf2fded3b2c99977df9ff68e6
mariona081 <xanfonm@gmail.com>
2025-07-25 14:59:58 +0300

    changed-sensitive-info

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,7 +1,5 @@
 import { createClient } from "@supabase/supabase-js"; 
 
-const supabaseUrl = 'https://ycljwqibrzwzpdfkgiqz.supabase.co'
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InljbGp3cWlicnp3enBkZmtnaXF6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTEzNzI3MTEsImV4cCI6MjA2Njk0ODcxMX0.KO03MyiO3vLhDM0UxLxED_1JmCYShw-cAIyqNL3Vv5s'
-
-
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_TOKEN
 export const supabase = createClient(supabaseUrl, supabaseAnonKey) 
